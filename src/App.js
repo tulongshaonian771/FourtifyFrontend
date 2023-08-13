8af686f269d6f5d76a27c4755ddf0a97f9215767
shordinger1 <1690809117@qq.com>
2023-08-13 10:26:17 +0800

    111

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import WholePage from "./WholePage";
+import SongListPage from "./LocationSongListPage";
+import MyProfile from "./MyProfile";
+import DefaultPage from "./DefaultPage";
+import LocationSongListPage from "./LocationSongListPage";
+import TimeSongListPage from "./TimeSongListPage";
+import PublicPage from "./PublicPage";
+import AfterLoginSongTable from "./AfterLoginSongTable";
+import AfterLoginSongList from "./AfterLoginSongList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const username = localStorage.getItem('username');
+    const password = localStorage.getItem('password');
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={username && password ? <WholePage /> : <DefaultPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/main" element={<WholePage />} />
+                <Route path="/locationlist" element={<LocationSongListPage />} />
+                <Route path="/myprofile" element={<MyProfile />} />
+                <Route path="/defaultpage" element={<DefaultPage />} />
+                <Route path="/timelist" element={<TimeSongListPage />} />
+                <Route path="/publiclist" element={<PublicPage />} />
+                <Route path="/after" element={<AfterLoginSongList />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
